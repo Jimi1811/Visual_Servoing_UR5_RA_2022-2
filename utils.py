8ce40c7a796ecb1c392741259e5c9cf210f901e6
Jim Fabián <40177903+Jimi1811@users.noreply.github.com>
2022-11-21 19:21:12 -0500

    Mejora de funciones

--- utils.py
+++ utils.py
@@ -21,65 +21,80 @@ def dh(d, theta, a, alpha):
                   [0.0,     0.0,     0.0,   1.0]])
     return T
 
-
 def fkine(q):
     """
     Calcular la cinematica directa del robot UR5 dados sus valores articulares. 
     q es un vector numpy de la forma [q1, q2, q3, q4, q5, q6]
-
     """
-    # Longitudes (en metros)
-
-    # Matrices DH (completar)
-    T1 = 0
-    T2 = 0
-    T3 = 0
-    T4 = 0
-    T5 = 0
-    T6 = 0
+    # Matrices DH
+    T1 = dh( 0.0892,        q[0],     0, pi/2)
+    T2 = dh(      0, q[1]+2*pi/2, 0.425,    0)
+    T3 = dh(      0,        q[2], 0.392,    0)
+    T4 = dh( 0.1093, q[3]+2*pi/2,     0, pi/2)
+    T5 = dh(0.09475,     q[4]+pi,     0, pi/2)
+    T6 = dh( 0.0825,        q[5],     0,    0)
     # Efector final con respecto a la base
-    T = 0
+    T = T1.dot(T2).dot(T3).dot(T4).dot(T5).dot(T6)
     return T
 
-
-def jacobian_position(q, delta=0.0001):
-    """
-    Jacobiano analitico para la posicion. Retorna una matriz de 3x6 y toma como
-    entrada el vector de configuracion articular q=[q1, q2, q3, q4, q5, q6]
-
-    """
-    # Alocacion de memoria
-    J = np.zeros((3,6))
+def jacobian_position(q, delta):
+    # delta=0.0001
+ 
+    # Crear una matriz 3x6
+    J = np.zeros((3, 6))
     # Transformacion homogenea inicial (usando q)
-    T = fkine(q)
+    To = fkine(q)
+    To = To[0:3, -1:] # vector posicion
+ 
     # Iteracion para la derivada de cada columna
-    for i in xrange(7):
-        # Copiar la configuracion articular inicial (usar este dq para cada
-        # incremento en una articulacion)
+    for i in range(6):
+        # Copiar la configuracion articular inicial
         dq = copy(q)
         # Incrementar la articulacion i-esima usando un delta
-
-        # Transformacion homogenea luego del incremento (q+dq)
-
+        dq[i] = dq[i]+delta
+        # Transformacion homogenea luego del incremento (q+delta)
+        T = fkine(dq)
+        T = T[0:3, -1:] # vector posicion
         # Aproximacion del Jacobiano de posicion usando diferencias finitas
-
+        Jq = 1/delta*(T-To)
+        J[:, i:i+1] = Jq
+       
     return J
 
-
-def jacobian_pose(q, delta=0.0001):
+def jacobian_pose(q, delta):
     """
     Jacobiano analitico para la posicion y orientacion (usando un
     cuaternion). Retorna una matriz de 7x6 y toma como entrada el vector de
     configuracion articular q=[q1, q2, q3, q4, q5, q6]
-
+    
     """
+ 
     J = np.zeros((7,6))
     # Implementar este Jacobiano aqui
-
-    
+    To = fkine(q)
+    x0 = TF2xyzquat(To)
+ 
+    # Iteracion para la derivada de cada columna
+    for i in range(6):
+        # Copiar la configuracion articular inicial
+        dq = copy(q)
+        # Incrementar la articulacion i-esima usando un delta
+        dq[i] = dq[i]+delta
+        # Transformacion homogenea luego del incremento (q+delta)
+        T = fkine(dq)
+        x = TF2xyzquat(T)
+        # Aproximacion del Jacobiano de posicion usando diferencias finitas
+        Jq = 1/delta*(x-x0)
+ 
+        J[0, i:i+1] = Jq[0]
+        J[1, i:i+1] = Jq[1]
+        J[2, i:i+1] = Jq[2]
+        J[3, i:i+1] = Jq[3]
+        J[4, i:i+1] = Jq[4]
+        J[5, i:i+1] = Jq[5]
+        J[6, i:i+1] = Jq[6]
+ 
     return J
-
-
 
 def rot2quat(R):
     """
